c8b96dca44d2fa665ed98e5bda357a9b2cd43fbf
Juanje <castelock@gmail.com>
2017-04-04 15:46:52 +0200

    SURF version2

--- SurfAlgorithm.py
+++ SurfAlgorithm.py
@@ -4,20 +4,40 @@ import numpy as np
 # Open Camera object
 cap = cv2.VideoCapture(0)
 
-# INCLUIR ESTO EN UN LOOP Y OBTENER UNA IMAGEN DE LA CÁMARA EN CADA FRAME PARA USAR SURF
-grayPic = cv2.cvtColor(cap,cv2.COLOR_BGR2GRAY)
-# Create the SURF descriptor
+
+# while True:
+#     # Grab the current frame
+#     (grabbed, frame) = cap.read()
+#
+#     grayPic = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # Create the SURF descriptor
+#     surf = cv2.xfeatures2d.SURF_create()
+#     # Get the keypoints
+#     keypoints,	descriptor	=	surf.detectAndCompute(grayPic,None)
+#     # Draw the keypoints
+#     grayPic = cv2.drawKeypoints(image=grayPic,	outImage=grayPic,	keypoints	=	keypoints,
+#     flags =	cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,	color	=	(51,	163,	236))
+#     # Show the image
+#     cv2.imshow('Surf Algorithm', grayPic)
+
+    # Grab the current frame
+(grabbed, frame) = cap.read()
+
+grayPic = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Create the SURF descriptor
 surf = cv2.xfeatures2d.SURF_create()
-# Get the keypoints
-keypoints,	descriptor	=	surf.detectAndCompute(grayPic,None)
-# Draw the keypoints
-grayPic = cv2.drawKeypoints(image=grayPic,	outImage=grayPic,	keypoints	=	keypoints,
-flags	=	cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINT,	color	=	(51,	163,	236))
+    # Get the keypoints
+keypoints, descriptor = surf.detectAndCompute(grayPic, None)
+    # Draw the keypoints
+grayPic = cv2.drawKeypoints(image=grayPic, outImage=grayPic, keypoints=keypoints,
+                            flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS, color=(51, 163, 236))
+    # Show the image
+cv2.imshow('Surf Algorithm', grayPic)
+cv2.imwrite('SURF_hand.png',grayPic)
 
-cv2.imshow('Surf Algorithm',grayPic)
-
-while(True):
-    if cv2.waitKey(1) & 0xff == ord("q"):
+while True:
+    # if the 'q' key is pressed, stop the loop
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 
